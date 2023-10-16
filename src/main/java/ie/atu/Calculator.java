aa740690d723310d030929f9e35fc5e14d118f93
Paul Lennon <paul.lennon@gmit.ie>
2023-10-16 09:36:14 +0100

    Added new method called main

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -1,7 +1,7 @@
 package ie.atu;
 
-public class Main {
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello");
     }
 }
